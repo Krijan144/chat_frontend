e05fe572c3e8fe140b72553add180658f1993bb2
sagun shrestha <shtsgn13@gmail.com>
2021-01-26 15:52:49 +0545

    chat app

---
+++ src/components/Join.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Join = () => {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+
+    const clickHandle = (e) => {
+        return (
+            (!name || !room) ? e.preventDefault() : null
+        )
+
+    }
+    return (
+        <div className="container d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
+            <div className="text-center">
+                <h1>Join</h1>
+
+                <div><input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} /></div><br />
+                {/* <div><input type="text" placeholder="room" onChange={(e) => setRoom(e.target.value)} /></div><br /> */}
+
+                <Link to={`/chat?name=${name}&room=${room}`} onClick={clickHandle}>
+                    <button>Join in</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Join
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Chat from "./components/chat";
+import Join from "./components/Join";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Route path='/' exact component={Join} />
       <Route path="/chat" component={Chat} />
     </BrowserRouter>
   );
